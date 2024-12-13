01a1c4f276266945a1929f9afb747db9f0c2f9d9
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-12-12 22:03:40 -0500

    chore: rename variable for clarity

--- src/integrations/shared/path-to-locale.ts
+++ src/integrations/shared/path-to-locale.ts
@@ -14,15 +14,15 @@ export function pathToLocale(
     astroConfig: { root: AstroConfig['root'], srcDir: AstroConfig['srcDir'] }
   },
 ): string | undefined {
-  const docsPath = getCollectionPath('pages', astroConfig.srcDir)
+  const pagesPath = getCollectionPath('pages', astroConfig.srcDir)
   // Format path to unix style path.
   path = path?.replace(/\\/g, '/')
   // Ensure that the page path starts with a slash if the docs directory also does,
   // which makes stripping the docs path in the next step work on Windows, too.
-  if (path && !path.startsWith('/') && docsPath.startsWith('/'))
+  if (path && !path.startsWith('/') && pagesPath.startsWith('/'))
     path = `/${path}`
   // Strip docs path leaving only content collection file ID.
   // Example: /Users/houston/repo/src/content/docs/en/guide.md => en/guide.md
-  const slug = path?.replace(docsPath, '')
+  const slug = path?.replace(pagesPath, '')
   return slugToLocale(slug, vitesseConfig)
 }
